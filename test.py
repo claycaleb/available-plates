74fb03d78d367018f1556f4cc58150fadc9c3560
claycaleb <48763589+claycaleb@users.noreply.github.com>
2019-05-21 20:22:13 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -2,16 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
 
 driver = webdriver.Chrome()
-
 
 driver.get("https://services.dps.ohio.gov/BMVOnlineServices/VR/Availability/Passenger/Check")
 
 NewPlate = driver.find_element_by_id('NewPlateNumber')
 Button = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[1]/div[2]/form/div[1]/div[2]/div/div[1]/div[2]/fieldset/div/button')
-wait = WebDriverWait(driver, 0)
+wait = WebDriverWait(driver, 0.7)
 
 for x in range(100, 1000):
     NewPlate.send_keys(x)
@@ -24,6 +22,7 @@ for x in range(100, 1000):
     print(x)
 
 # to split the string...
+# copy/paste results into s
 # s = 'string'
 # n = 3
 # sl = [s[i:i+n] for i in range(0, len(s), n)]
